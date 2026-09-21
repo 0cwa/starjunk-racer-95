6f0949293ec616f18614c89a6bbe91779c4fd011
0cwa <kainau@yahoo.com>
2026-09-21 11:12:51 +0000

    Disable unsupported WebGPU mobile subpasses

--- tools/engine/apply_starjunk_port_patches.py
+++ tools/engine/apply_starjunk_port_patches.py
@@ -29,7 +29,7 @@ def main() -> None:
     context_implementation = source / "drivers/webgpu/rendering_context_driver_webgpu.cpp"
     platform_header = source / "drivers/webgpu/webgpu_platform.h"
     main_implementation = source / "main/main.cpp"
-    tone_mapper_implementation = source / "servers/rendering/renderer_rd/effects/tone_mapper.cpp"
+    forward_mobile_implementation = source / "servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.cpp"
 
     replace_once(
         device_implementation,
@@ -83,46 +83,16 @@ static WGPUTextureFormat starjunk_webgpu_storage_format(
     )
 
     replace_once(
-        tone_mapper_implementation,
-        """#include "tone_mapper.h"
-
-#include "servers/rendering/renderer_rd/renderer_compositor_rd.h"
+        forward_mobile_implementation,
+        """\tbool using_subpass_post_process = true; // If true: we can do our post processing in a subpass
 """,
-        """#include "tone_mapper.h"
-
-#include "core/os/os.h"
-#include "servers/rendering/renderer_rd/renderer_compositor_rd.h"
-""",
-    )
-
-    replace_once(
-        tone_mapper_implementation,
-        """\tRID default_sampler = material_storage->sampler_rd_get_default(RSE::CANVAS_ITEM_TEXTURE_FILTER_LINEAR, RSE::CANVAS_ITEM_TEXTURE_REPEAT_DISABLED);
-\tRID default_mipmap_sampler = material_storage->sampler_rd_get_default(RSE::CANVAS_ITEM_TEXTURE_FILTER_LINEAR_WITH_MIPMAPS, RSE::CANVAS_ITEM_TEXTURE_REPEAT_DISABLED);
-
-\tRD::Uniform u_source_color;
-\tu_source_color.uniform_type = RD::UNIFORM_TYPE_INPUT_ATTACHMENT;
-\tu_source_color.binding = 0;
-\tu_source_color.append_id(p_source_color);
-""",
-        """\tRID default_sampler = material_storage->sampler_rd_get_default(RSE::CANVAS_ITEM_TEXTURE_FILTER_LINEAR, RSE::CANVAS_ITEM_TEXTURE_REPEAT_DISABLED);
-\tRID default_mipmap_sampler = material_storage->sampler_rd_get_default(RSE::CANVAS_ITEM_TEXTURE_FILTER_LINEAR_WITH_MIPMAPS, RSE::CANVAS_ITEM_TEXTURE_REPEAT_DISABLED);
-\tRID nearest_sampler = material_storage->sampler_rd_get_default(RSE::CANVAS_ITEM_TEXTURE_FILTER_NEAREST, RSE::CANVAS_ITEM_TEXTURE_REPEAT_DISABLED);
-
-\tRD::Uniform u_source_color;
-\tif (OS::get_singleton()->get_current_rendering_driver_name() == "webgpu") {
-\t\t// The bridge fork disables SUBPASS in the WebGPU tonemap shader because
-\t\t// WebGPU has no Vulkan-style input attachments. Match the resulting
-\t\t// combined-sampler shader contract while the driver emulates subpasses.
-\t\tu_source_color.uniform_type = RD::UNIFORM_TYPE_SAMPLER_WITH_TEXTURE;
-\t\tu_source_color.binding = 0;
-\t\tu_source_color.append_id(p_settings.bilinear_filtering ? default_sampler : nearest_sampler);
-\t\tu_source_color.append_id(p_source_color);
-\t} else {
-\t\tu_source_color.uniform_type = RD::UNIFORM_TYPE_INPUT_ATTACHMENT;
-\t\tu_source_color.binding = 0;
-\t\tu_source_color.append_id(p_source_color);
-\t}
+        """\t// The bridge WebGPU backend does not implement render subpasses: its
+\t// command_next_render_subpass() is a no-op and render_pass_create() ignores
+\t// subpass descriptions. Use Forward Mobile's existing separate post-pass
+\t// path until the full WebGPU subpass emulation from the polished reference
+\t// is forward-ported.
+\tbool using_subpass_post_process =
+\t\t\tRD::get_singleton()->get_device_capabilities().device_family != RDD::DEVICE_WEBGPU;
 """,
     )
 
